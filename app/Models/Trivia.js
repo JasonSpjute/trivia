14ca6d7267d011dbf9c2bb0b6d855b06d73352c0
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-01-04 21:09:41 -0700

    Colors

--- app/Models/Trivia.js
+++ app/Models/Trivia.js
@@ -11,14 +11,14 @@ export default class Trivia{
         return /*html*/`
             <div class="col-4">
                 <div class = "card my-3 border-primary shadow">
-                    <div class = "card-header bg-primary">
-                        <h2><b>CATEGORY:</b> ${this.category}</h2>
+                    <div class = "card-header bg-success">
+                        <h1><b>CATEGORY</b> ${this.category}</h1>
                     </div>
                     <div class = "card-body">
                         <h4>${this.question}</h4>
                         <div class="text-center">
-                        <button type="button" class="btn btn-dark" ${this.correct == "True" ? `onclick="alert('You are correct! the answer is ${this.correct}.')"` : `onclick="alert('You are wrong! the answer is ${this.correct}.')"`}>True</button>
-                        <button type="button" class="btn btn-warning" ${this.correct == "False" ? `onclick="alert('You are correct! The answer is ${this.correct}.')"` : `onclick="alert('You are wrong! The answer is ${this.correct}.')"`}>False</button>
+                        <button type="button" class="btn btn-secondary" ${this.correct == "True" ? `onclick="alert('You are correct! the answer is ${this.correct}.')"` : `onclick="alert('You are wrong! the answer is ${this.correct}.')"`}>True</button>
+                        <button type="button" class="btn btn-danger" ${this.correct == "False" ? `onclick="alert('You are correct! The answer is ${this.correct}.')"` : `onclick="alert('You are wrong! The answer is ${this.correct}.')"`}>False</button>
                         </div>
                     </div>
                 </div>
